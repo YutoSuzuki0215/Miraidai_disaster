44d766aaf81d53fac9cd367b5ec3e98073641b68
YutoSuzuki0215 <69447114+YutoSuzuki0215@users.noreply.github.com>
2021-01-19 09:52:38 +0900

    一応完成

---
+++ Assets/GoalManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoalManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject text1;
+    public GameObject text2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == player.name)
+        {
+            text1.SetActive(false);
+            text2.GetComponent<Text>().text = "到着！！";
+            text2.SetActive(true);
+            Invoke("ChangeScene",1.5f);
+        }
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+}
